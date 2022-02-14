87be4daba15adac26053febffcf067fbae9ea9d2
GotLed <61875331+camille-marro@users.noreply.github.com>
2022-02-14 15:41:57 +0100

    Change path for files

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let fs = require('fs');
 
-let rawConfig = fs.readFileSync("config.json");
+let rawConfig = fs.readFileSync("json_files/config.json");
 let config = JSON.parse(rawConfig);
 exports.config = config;
