e51d92071552486bcf19a98c77827096c1708313
GotLed <61875331+camille-marro@users.noreply.github.com>
2022-04-25 10:54:32 +0200

    Change config.js
    config.js now exports new functions for logging and updating the configuration

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,25 @@
 let fs = require('fs');
 
 let rawConfig = fs.readFileSync("json_files/config.json");
-let config = JSON.parse(rawConfig);
-exports.config = config;
+const config = JSON.parse(rawConfig);
+
+function printConfig () {
+    console.log("Configuration : ")
+    console.log("- prefix : " + config['prefix']);
+    console.log("- language : " + config['lang']);
+    console.log("----------------");
+}
+
+function changeConfig(newConfig) {
+    let JSONConfig = JSON.stringify(newConfig);
+    fs.writeFileSync("json_files/config.json", JSONConfig);
+
+    console.log("|- changing configuration for : ");
+    printConfig();
+}
+
+module.exports = {
+    config,
+    printConfig,
+    changeConfig
+};
