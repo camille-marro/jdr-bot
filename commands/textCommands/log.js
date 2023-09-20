f7bfe5843c280d6aaf5dc555017d8e3a533a892b
camille-marro <>
2023-09-20 17:30:01 +0200

    added clearlog command

--- commands/textCommands/log.js
+++ commands/textCommands/log.js
@@ -2,6 +2,7 @@ let fs = require('fs');
 const path = require("path");
 
 let log = require('../../assets/log');
+const {EmbedBuilder} = require("discord.js");
 
 function printLog(length, message) {
     if (length === undefined) length = 20;
@@ -17,13 +18,22 @@ function printLog(length, message) {
     message.channel.send(msgToSend);
 }
 
+function clearLog(message) {
+    fs.writeFileSync(path.resolve(__dirname, "../../logs.txt"), "");
+    let msgEmbed = new EmbedBuilder();
+    msgEmbed.setColor("#0cb200");
+    msgEmbed.setTitle("Logs supprimées avec succès");
+    message.channel.send({embeds: [msgEmbed]});
+    log.print("resetting logs", message.author);
+}
+
 function execute(message) {
     let args = message.content.split(" ");
 
     if (args[1] === "print") {
         printLog(args[2], message);
-    } else {
-
+    } else if (args[1] === "clear") {
+        clearLog(message);
     }
 }
 
