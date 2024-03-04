29e5e079cc0013954d8d8a3cfc0748bce7dbd49f
GotLed <61875331+camille-marro@users.noreply.github.com>
2024-03-04 13:44:47 +0100

    added release command

--- commands/textCommands/pokemon.js
+++ commands/textCommands/pokemon.js
@@ -1780,6 +1780,16 @@ function getPlayerTeamsPokemonsWithName(player, pokemonName) {
  */
 function addToTeam(player, pokemonName, message) {
     selectPokemonToAdd(player, pokemonName, message).then((pokemonSelected, rej) => {
+        if (!pokemonSelected) {
+            let msgEmbed = new EmbedBuilder();
+            msgEmbed.setTitle("Vous n'avez aucun pokémon de ce nom !");
+            msgEmbed.setDescription("Vérifier l'orthographe du nom ou votre liste de pokémon avec la commande *pokemon list* !");
+            msgEmbed.setColor("#ff0000");
+            msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande pokemon help."});
+
+            message.channel.send({embeds: [msgEmbed]});
+            return;
+        }
         let msgEmbed = addPokemonToPlayerTeam(player, pokemonSelected);
         message.channel.send({embeds: [msgEmbed]});
         updateData();
@@ -1815,10 +1825,21 @@ async function createTeam(player, message) {
     let pokemonsToSelect = args.slice(3);
     player["team"] = [];
     for (const pokemonName of pokemonsToSelect) {
+
         selectPokemonToAdd(player, pokemonName, message).then((pokemonSelected, rej) => {
-            let msgEmbed = addPokemonToPlayerTeam(player, pokemonSelected);
-            message.channel.send({embeds: [msgEmbed]});
-            updateData();
+            if (!pokemonSelected) {
+                let msgEmbed = new EmbedBuilder();
+                msgEmbed.setTitle("Vous n'avez aucun pokémon de ce nom !");
+                msgEmbed.setDescription("Vérifier l'orthographe du nom ou votre liste de pokémon avec la commande *pokemon list* !");
+                msgEmbed.setColor("#ff0000");
+                msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande pokemon help."});
+
+                message.channel.send({embeds: [msgEmbed]});
+            } else {
+                let msgEmbed = addPokemonToPlayerTeam(player, pokemonSelected);
+                message.channel.send({embeds: [msgEmbed]});
+                updateData();
+            }
         });
     }
 }
@@ -1970,6 +1991,104 @@ function createTeamMessage(message) {
     message.channel.send({embeds: [msgEmbed]});
 }
 
+function releasePokemonMain(message) {
+    let player = getPlayerWithId(message.author.id);
+    let args = message.content.split(" ");
+
+    if (!player) {
+        let msgEmbed = new EmbedBuilder();
+        msgEmbed.setTitle("Vous n'avez pas de compte !");
+        msgEmbed.setDescription("Pour vous inscrire utilisez la commande *pokemon start* !");
+        msgEmbed.setColor("#ff0000");
+        msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande pokemon help."});
+
+        message.channel.send({embeds: [msgEmbed]});
+        return;
+    }
+
+    selectPokemonToRelease(player, args[2], message).then((pokemonSelected, rej) => {
+        if (!pokemonSelected) {
+            let msgEmbed = new EmbedBuilder();
+            msgEmbed.setTitle("Vous n'avez aucun pokémon de ce nom !");
+            msgEmbed.setDescription("Vérifier l'orthographe du nom ou votre liste de pokémon avec la commande *pokemon list* !");
+            msgEmbed.setColor("#ff0000");
+            msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande pokemon help."});
+
+            message.channel.send({embeds: [msgEmbed]});
+            return;
+        }
+        let isReleased = releasePokemon(player, pokemonSelected);
+        let msgEmbed = new EmbedBuilder();
+        msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
+        if (isReleased) {
+            msgEmbed.setTitle("Votre " + pokemonSelected.name + " a été relaché !");
+            msgEmbed.setDescription("Adieu " + pokemonSelected.name + " !");
+            msgEmbed.setColor("#309393");
+        } else {
+            msgEmbed.setTitle("Une erreur est survenue ! ");
+            msgEmbed.setColor("#ff0000");
+        }
+        message.channel.send({embeds: [msgEmbed]});
+        updateData();
+    });
+}
+
+function releasePokemon(player, pokemon) {
+    let i = 0;
+    while (i < player["pokemons"].length) {
+        if (comparePokemon(player["pokemons"][i], pokemon)) {
+            player["pokemons"].splice(i, 1);
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+async function selectPokemonToRelease(player, pokemonName, message) {
+    return new Promise(async (resolve, reject) => {
+        let pokemons = getPlayerTeamsPokemonsWithName(player, pokemonName);
+        if (pokemons.length > 1) {
+            // choisir le pokemon à ajouter
+            let msgEmbed = createEmbedCreateTeam(pokemons);
+            let msgSent = await message.channel.send({embeds: [msgEmbed]});
+
+            for (let i = 0; i < pokemons.length; i++) {
+                await msgSent.react(emojis[i]);
+            }
+
+            const filter = (reaction, user) => {
+                return emojis.includes(reaction.emoji.name) && !user.bot;
+            };
+
+            let collector = msgSent.createReactionCollector(filter, {time: 15000});
+
+            collector.on('collect', (reaction, user) => {
+                if (user.id === message.author.id) {
+                    let i = 0;
+                    while (i < pokemons.length) {
+                        if (reaction.emoji.name === emojis[i]) {
+                            collector.stop();
+                            resolve(pokemons[i]);
+                            return;
+                        }
+                        i++;
+                    }
+                } else if (!user.bot) {
+                    let msgEmbed = new EmbedBuilder();
+                    msgEmbed.setTitle("Vous ne pouvez pas réagir aux messages des autres !");
+                    msgEmbed.setDescription("<@" + user.id + "> fait plus ça c'est pas bien !");
+                    msgEmbed.setColor("#ff0000");
+                    msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande pokemon help."});
+
+                    message.channel.send({embeds: [msgEmbed]});
+                }
+            });
+
+        } else resolve(pokemons[0]);
+    });
+}
+
 function execute(message) {
     let args = message.content.split(" ");
     if (args[1] === "test") {
@@ -1992,6 +2111,8 @@ function execute(message) {
         healPokemons(message);
     } else if (args[1] === "team") {
         teamManager(message);
+    } else if (args[1] === "release") {
+        releasePokemonMain(message);
     } else if (args[1] === "admin") {
         admin(message);
     }
