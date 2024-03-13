0df2415ed3212a6ae709f980203f73af10c4bd74
GotLed <61875331+camille-marro@users.noreply.github.com>
2024-03-13 03:53:01 +0100

    reworked some of combatpve functions

--- commands/textCommands/pokemon/combatPVE.js
+++ commands/textCommands/pokemon/combatPVE.js
@@ -1,144 +1,159 @@
 const {EmbedBuilder} = require("discord.js");
 
 const {getPlayerTeam, addExp, comparePokemonUUID, updateData} = require("./assets");
+const {emojis} = require("./utils");
 
-function startCombatPVE(player, enemyPokemon, message) {
-    console.log("starting a pve combat against " + enemyPokemon.name);
-    let team = getPlayerTeam(player);
-    // check si un pokémon dans la team
+function startCombatPVE(player, enemyPokemon, message, titleStr = "Vous croisez un pokémon sauvage !", canEscape = true) {
+    return new Promise((resolve) => {
+        // console.log("starting a pve combat against " + enemyPokemon.name);
+        let team = getPlayerTeam(player);
+        // check si un pokémon dans la team
 
-    let combatObject = {
-        "playerTurn": true,
-        "pokemonsSent": [0],
-        "pokemonSent": team[0],
-        "fuite": false,
-        "fuiteCpt": 0
-    }
+        let combatObject = {
+            "playerTurn": true,
+            "pokemonsSent": [0],
+            "pokemonSent": team[0],
+            "fuite": false,
+            "fuiteCpt": 0
+        }
 
-    let msgEmbed = new EmbedBuilder();
-    msgEmbed.setTitle("Vous croisez un pokémon sauvage !");
-    msgEmbed.setColor("#00ce5e");
-    msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
-    msgEmbed.addFields({name: enemyPokemon.name, value: "Niveau " + enemyPokemon.level});
-    message.channel.send({embeds: [msgEmbed]});
+        let msgEmbed = new EmbedBuilder();
+        msgEmbed.setTitle(titleStr);
+        msgEmbed.setColor("#00ce5e");
+        msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
+        msgEmbed.addFields({name: enemyPokemon.name, value: "Niveau " + enemyPokemon.level});
+        message.channel.send({embeds: [msgEmbed]});
 
-    combatPVE(player, enemyPokemon, team, combatObject, message).then(res => {
-        console.log("fin du combat !");
-        console.log(res);
-        if (res === "win") {
-            let msgEmbed = new EmbedBuilder();
-            msgEmbed.setTitle("Victoire ! " + enemyPokemon.name + " ennemi est K.O. !");
-            msgEmbed.setColor("#fff300");
-            msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
-            message.channel.send({embeds: [msgEmbed]});
+        combatPVE(player, enemyPokemon, team, combatObject, message, canEscape).then(res => {
+            // console.log("fin du combat !");
+            if (res === "win") {
+                let msgEmbed = new EmbedBuilder();
+                msgEmbed.setTitle("Victoire ! " + enemyPokemon.name + " ennemi est K.O. !");
+                msgEmbed.setColor("#fff300");
+                msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
+                message.channel.send({embeds: [msgEmbed]});
 
-            rewardCombat(team, enemyPokemon, combatObject, message).then(rewardResult => {
+                rewardCombat(team, enemyPokemon, combatObject, message).then(rewardResult => {
 
-                if (rewardResult[0].length > 1) {
-                    let i = 0;
-                    while (i < rewardResult[0].length) {
+                    if (rewardResult[0].length > 1) {
+                        let i = 0;
+                        while (i < rewardResult[0].length) {
+                            let msgEmbed = new EmbedBuilder();
+                            msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
+                            msgEmbed.setColor("#00ce5e");
+                            let pokemon = team[combatObject["pokemonsSent"][i]];
+                            let xpWon = Math.ceil(rewardResult[1] / combatObject["pokemonsSent"].length)
+                            msgEmbed.setTitle(pokemon.name + " a gagné " + xpWon + " xp");
+                            if (rewardResult[0][i] > 0) msgEmbed.addFields({name: "Niveau(x) gagné(s)", value: rewardResult[0][i].toString()});
+                            message.channel.send({embeds: [msgEmbed]});
+                            i++;
+                        }
+                        // console.log(rewardResult);
+                    } else {
                         let msgEmbed = new EmbedBuilder();
                         msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
                         msgEmbed.setColor("#00ce5e");
-                        let pokemon = team[combatObject["pokemonsSent"][i]];
-                        let xpWon = Math.ceil(rewardResult[1] / combatObject["pokemonsSent"].length)
-                        msgEmbed.setTitle(pokemon.name + " a gagné " + xpWon + " xp");
-                        if (rewardResult[0][i] > 0) msgEmbed.addFields({name: "Niveau(x) gagné(s)", value: rewardResult[0][i].toString()});
+                        msgEmbed.setTitle(team[0].name + " a gagné " + rewardResult[1] + " xp");
+                        if (rewardResult[0][0] > 0) msgEmbed.addFields({name: "Niveau(x) gagné(s)", value: rewardResult[0][0].toString()});
                         message.channel.send({embeds: [msgEmbed]});
-                        i++;
+                        // console.log(rewardResult);
                     }
-                    console.log(rewardResult);
-                } else {
-                    let msgEmbed = new EmbedBuilder();
-                    msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
-                    msgEmbed.setColor("#00ce5e");
-                    msgEmbed.setTitle(team[0].name + " a gagné " + rewardResult[1] + " xp");
-                    if (rewardResult[0][0] > 0) msgEmbed.addFields({name: "Niveau(x) gagné(s)", value: rewardResult[0][0].toString()});
-                    message.channel.send({embeds: [msgEmbed]});
-                    console.log(rewardResult);
-                }
+                });
+                resolve("win");
+            } else if (res === "fuite") {
+                let msgEmbed = new EmbedBuilder();
+                msgEmbed.setTitle("Vous avez réussi à fuire le combat !");
+                msgEmbed.setColor("#ffffff");
+                msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
+                message.channel.send({embeds: [msgEmbed]});
+                // console.log("Gros looser t'as fuit ...");
+                resolve("fuite");
+            } else if (res === "defeat") {
+                let msgEmbed = new EmbedBuilder();
+                msgEmbed.setTitle("Défaite ! " + enemyPokemon.name + " a réussi à mettre K.O. tous vos pokémons");
+                msgEmbed.setColor("#ff0000");
+                msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
+                message.channel.send({embeds: [msgEmbed]});
+                // console.log("Gros looser t'as perdu en 1000 vs 1");
+                resolve("defeat");
+            }
 
-            });
-        } else if (res === "fuite") {
-            let msgEmbed = new EmbedBuilder();
-            msgEmbed.setTitle("Vous avez réussi à fuire le combat !");
-            msgEmbed.setColor("#ffffff");
-            msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
-            message.channel.send({embeds: [msgEmbed]});
-            console.log("Gros looser t'as fuit ...");
-        } else if (res === "defeat") {
-            let msgEmbed = new EmbedBuilder();
-            msgEmbed.setTitle("Défaite ! " + enemyPokemon.name + " a réussi à mettre K.O. tous vos pokémons");
-            msgEmbed.setColor("#ff0000");
-            msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"pokemon help\"."});
-            message.channel.send({embeds: [msgEmbed]});
-            console.log("Gros looser t'as perdu en 1000 vs 1");
-        }
-
-        updateData();
+            updateData();
+        });
     });
+
 }
 
-function combatPVE(player, enemyPokemon, playerTeam, combatObject, message) {
-    console.log("Starting a turn !");
-    console.log(combatObject);
+function combatPVE(player, enemyPokemon, playerTeam, combatObject, message, canEscape = true) {
+    // console.log("Starting a turn !");
+    // console.log(combatObject);
     return new Promise(async (resolve, reject) => {
-        console.log("Checking win or lose conditions");
+        // console.log("Checking win or lose conditions");
         if (enemyPokemon["currentHP"] <= 0) {
-            console.log("enemy pokemon dead, ending turn ...");
+            // console.log("enemy pokemon dead, ending turn ...");
             resolve("win");
             return;
         } else if (checkPlayerLose(playerTeam)) {
-            console.log("player lose, ending turn ...");
+            // console.log("player lose, ending turn ...");
             resolve("defeat");
             return;
         } else if (combatObject["fuite"]) {
-            console.log("player escape, ending turn");
+            // console.log("player escape, ending turn");
             resolve("fuite");
             return;
         }
-        console.log("no win/lose condition, continuing");
+        // console.log("no win/lose condition, continuing");
 
         let attackResult, titleStr;
         if (combatObject["playerTurn"]) {
-            console.log("player turn !");
+            // console.log("player turn !");
             let attack = await chooseAttack(combatObject, message);
             if (attack === "fuite") {
+                if (!canEscape) {
+                    let msgEmbed = new EmbedBuilder();
+                    msgEmbed.setTitle("Vous ne pouvez pas vous enfuir !");
+                    msgEmbed.setDescription("Il est impossible de s'enfuir pendant ce combat !");
+                    msgEmbed.setColor("#ff0000");
+                    msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande pokemon help."});
+
+                    message.channel.send({embeds: [msgEmbed]});
+                    resolve(combatPVE(player, enemyPokemon, playerTeam, combatObject, message));
+                }
                 if(tryEscape(combatObject, enemyPokemon)) {
                     combatObject["fuite"] = true;
                 } else {
                     combatObject["fuiteCpt"]++;
                 }
                 combatObject["playerTurn"] = !combatObject["playerTurn"];
-                console.log("ending player turn.");
+                // console.log("ending player turn.");
                 resolve(combatPVE(player, enemyPokemon, playerTeam, combatObject, message));
                 return;
             } else if (attack === "changePokemon") {
                 let changePokemonRes = await changePokemon(playerTeam, combatObject, message);
                 if (changePokemonRes) combatObject["playerTurn"] = !combatObject["playerTurn"];
-                console.log("ending player turn.");
+                // console.log("ending player turn.");
                 resolve(combatPVE(player, enemyPokemon, playerTeam, combatObject, message));
                 return;
             } else if (!attack) {
-                console.log("aled ?");
+                // console.log("aled ?");
                 resolve(false);
                 return;
             }
             attackResult = attackPokemon(combatObject["pokemonSent"], enemyPokemon, attack);
             titleStr = combatObject["pokemonSent"].name + " utilise " + attack.name + ".";
         } else {
-            console.log("wild pokemon turn !");
+            // console.log("wild pokemon turn !");
             let attack = enemyPokemon["capacities"][Math.floor(Math.random() * enemyPokemon["capacities"].length)];
 
-            console.log("wild pokemon use : " + attack.name);
-            console.log(attack);
+            // console.log("wild pokemon use : " + attack.name);
+            // console.log(attack);
             attackResult = attackPokemon(enemyPokemon, combatObject["pokemonSent"], attack);
             titleStr = enemyPokemon.name + " utilise " + attack.name + ".";
         }
 
         let msgEmbed = new EmbedBuilder();
         if (attackResult["cc"]) {
-            console.log("Coup critique !");
+            // console.log("Coup critique !");
             titleStr += " Coup Critique !"
             msgEmbed.setColor("#fff300");
         } else msgEmbed.setColor("#ff5600");
@@ -146,12 +161,12 @@ function combatPVE(player, enemyPokemon, playerTeam, combatObject, message) {
         if (attackResult["avantageType"] === 0) {
             if (combatObject["playerTurn"]) titleStr += ` Cela n'affecte pas ${enemyPokemon.name} ... `;
             else titleStr += ` Cela n'affecte pas ${combatObject["pokemonSent"].name} ... `;
-            console.log("Immunité !");
+            // console.log("Immunité !");
         } else if (attackResult["avantageType"] < 1) {
-            console.log("Ce n'est pas très efficace ...");
+            // console.log("Ce n'est pas très efficace ...");
             titleStr += " Ce n'est pas très efficace ... ";
         } else if (attackResult["avantageType"] > 1) {
-            console.log("C'est très efficace");
+            // console.log("C'est très efficace");
             titleStr += " C'est super efficace ! ";
         }
 
@@ -162,15 +177,15 @@ function combatPVE(player, enemyPokemon, playerTeam, combatObject, message) {
         msgEmbed.addFields({name:enemyPokemon["name"], value:getStringHPLeft(enemyPokemon["currentHP"], enemyPokemon["stats"][0]), inline: true});
 
         message.channel.send({embeds: [msgEmbed]});
-        console.log("damage : " + attackResult["damage"]);
-        console.log("ending turn !");
+        // console.log("damage : " + attackResult["damage"]);
+        // console.log("ending turn !");
         combatObject["playerTurn"] = !combatObject["playerTurn"];
         resolve(combatPVE(player, enemyPokemon, playerTeam, combatObject, message));
     });
 }
 
 function changePokemon(playerTeam, combatObject, message) {
-    console.log("Changement du pokémon !");
+    // console.log("Changement du pokémon !");
     return new Promise(async (resolve, reject) => {
         let msgEmbed = new EmbedBuilder();
         msgEmbed.setTitle("Choisissez le pokémon à envoyer au combat");
@@ -211,13 +226,13 @@ function changePokemon(playerTeam, combatObject, message) {
                 while (i <= playerTeam.length) {
                     if (reaction.emoji.name === emojis[i]) {
                         if (i === playerTeam.length) {
-                            console.log("Annulation du changement de pokémon");
+                            // console.log("Annulation du changement de pokémon");
                             collector.stop();
                             resolve(false);
                             return;
                         }
                         if (playerTeam[i]["currentHP"] <= 0) {
-                            console.log("Le pokémon est KO on refait");
+                            // console.log("Le pokémon est KO on refait");
                             let msgEmbed = new EmbedBuilder();
                             msgEmbed.setTitle("Vous ne pouvez pas envoyer un pokémon K.O. au combat !");
                             msgEmbed.setColor("#ff0000");
@@ -228,7 +243,7 @@ function changePokemon(playerTeam, combatObject, message) {
                             resolve(changePokemon(playerTeam, combatObject, message));
                             return;
                         } else if (comparePokemonUUID(combatObject["pokemonSent"], playerTeam[i])) {
-                            console.log("Le pokémon est déjà au combat on refait !");
+                            // console.log("Le pokémon est déjà au combat on refait !");
                             let msgEmbed = new EmbedBuilder();
                             msgEmbed.setTitle("Ce pokémon est déjà en combat !");
                             msgEmbed.setColor("#ff0000");
@@ -239,7 +254,7 @@ function changePokemon(playerTeam, combatObject, message) {
                             resolve(changePokemon(playerTeam, combatObject, message));
                             return;
                         } else {
-                            console.log("On envoie le pokemon " + playerTeam[i].name);
+                            // console.log("On envoie le pokemon " + playerTeam[i].name);
                             combatObject["pokemonSent"] = playerTeam[i];
                             if (!combatObject["pokemonsSent"].includes(i)) combatObject["pokemonsSent"].push(i);
                             collector.stop();
@@ -324,25 +339,25 @@ function attackPokemon(pokemon, enemyPokemon, attack) {
 }
 
 function tryEscape(combatObject, enemyPokemon, multi = 1) {
-    console.log("trying to escape");
+    // console.log("trying to escape");
     let playerPokemon = combatObject["pokemonSent"];
     let A = playerPokemon["stats"][5]*32;
     let B = Math.floor(enemyPokemon["stats"][5]/4)%255;
     let chance = (Math.floor(A/B)+30)*multi;
 
     if (chance > 255) {
-        console.log("fuite réussie !")
+        // console.log("fuite réussie !")
         return true
     }
 
     let rand = Math.floor(Math.random() * 256);
 
-    console.log("fuite : " + rand <= chance);
+    // console.log("fuite : " + rand <= chance);
     return rand <= chance;
 }
 
 function chooseAttack(combatObject, message) {
-    console.log("choosing attack");
+    // console.log("choosing attack");
     return new Promise(async (resolve, reject) => {
         let pokemon = combatObject["pokemonSent"];
 
@@ -376,12 +391,12 @@ function chooseAttack(combatObject, message) {
         collector.on("collect", (reaction, user) => {
             if (user.id === message.author.id) {
                 if (reaction.emoji.name === emojis[pokemon["capacities"].length]) {
-                    console.log("escape selected !");
+                    // console.log("escape selected !");
                     resolve("fuite");
                     collector.stop();
                     return;
                 } else if (reaction.emoji.name === emojis[pokemon["capacities"].length+1]) {
-                    console.log("change pokemon selected !");
+                    // console.log("change pokemon selected !");
                     resolve("changePokemon");
                     collector.stop();
                     return;
@@ -390,7 +405,7 @@ function chooseAttack(combatObject, message) {
                 let i = 0;
                 while (i < 4) {
                     if (reaction.emoji.name === emojis[i]) {
-                        console.log(pokemon["capacities"][i].name + " selected");
+                        // console.log(pokemon["capacities"][i].name + " selected");
                         resolve(pokemon["capacities"][i]);
                         collector.stop();
                         return;
@@ -398,7 +413,7 @@ function chooseAttack(combatObject, message) {
                     i++;
                 }
 
-                console.log("unknown emoji");
+                // console.log("unknown emoji");
                 resolve(false);
                 collector.stop();
             } else if (!user.bot) {
