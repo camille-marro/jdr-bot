f23d4f445910d3e73929f6288b991f61c5d5039a
GotLed <61875331+camille-marro@users.noreply.github.com>
2024-06-17 21:06:08 +0200

    upgrade message system

--- commands/textCommands/imagine/change_config.js
+++ commands/textCommands/imagine/change_config.js
@@ -19,28 +19,28 @@ function change_config(message) {
             return;
         }
         parameters["width"] = parseInt(args[3]);
-        message.channel.send("Largeur mise à jour !");
+        sendSuccessMessage("Largeur modifiée", message);
     } else if (args[2] === "height") {
         if (!args[3]) {
             sendErrorMessage("Spécifiez une hauteur !", message);
             return;
         }
         parameters["height"] = parseInt(args[3]);
-        message.channel.send("Hauteur mise à jour !");
+        sendSuccessMessage("Hauteur modifiée", message);
     } else if (args[2] === "steps") {
         if (!args[3]) {
             sendErrorMessage("Spécifiez les sampling steps !", message);
             return;
         }
         parameters["steps"] = parseInt(args[3]);
-        message.channel.send("Sampling steps mis à jour !");
+        sendSuccessMessage("Sampling steps modifiés", message);
     } else if (args[2] === "cfg_scale") {
         if (!args[3]) {
             sendErrorMessage("Spécifiez le CFG Scale !", message);
             return;
         }
         parameters["cfg_scale"] = parseFloat(args[3]);
-        message.channel.send("CFG Scale mis à jour !");
+        sendSuccessMessage("CFG Scale modifié", message);
     } else if (args[2] === "sampler_name") {
         if (!args[3]) {
             sendErrorMessage("Spécifiez le sampler !", message);
@@ -52,7 +52,7 @@ function change_config(message) {
         }
         name = name.slice(0, -1);
         parameters["sampler_name"] = name;
-        message.channel.send("Sampler mis à jour !");
+        sendSuccessMessage("Sampler modifié", message);
     } else if (args[2] === "model") {
         if (!args[3]) {
             sendErrorMessage("Spécifiez un model !", message);
@@ -67,17 +67,21 @@ function change_config(message) {
                     if (model["model_name"] === model_name) title = model["title"];
                 });
                 if (!title) {
-                    message.channel.send("Model introuvable !");
+                    sendErrorMessage("Model introuvable !", message)
                     return;
                 }
 
-                message.channel.send("Changing model ...");
+                let msgEmbed = new EmbedBuilder();
+                msgEmbed.setColor("#0293af");
+                msgEmbed.setTitle("Chargement du nouveau modèle en cours ...");
+                msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"imagine help\""});
+
+                message.channel.send({embeds: [msgEmbed]});
 
                 axios.post("http://127.0.0.1:7860/sdapi/v1/options",
                     {"sd_model_checkpoint": title})
                     .then(res => {
-                        console.log(res);
-                        message.channel.send("Model chargé avec succès !");
+                        sendSuccessMessage("Model modifié", message);
                     })
                     .catch(e => {
                         console.error(e);
@@ -86,9 +90,21 @@ function change_config(message) {
             .catch(e => {
                 console.error(e);
             });
+    } else {
+        sendErrorMessage("Paramètre introuvable!", message);
     }
 
     updateData(parameters);
+}
+
+function sendSuccessMessage(text, message) {
+    let msgEmbed = new EmbedBuilder();
+    msgEmbed.setColor("#08ff00");
+    msgEmbed.setTitle(text + " avec succès !");
+    msgEmbed.setFooter({text: "Pour plus d'informations utilisez la commande \"imagine help\""});
+
+    message.channel.send({embeds: [msgEmbed]});
+    log.print("Sending success message", 1);
 }
 
 function sendErrorMessage(text, message) {
