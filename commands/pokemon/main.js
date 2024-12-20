1e2f1dd6c213a06cb69d3ba0718dca431b5ecef1
GotLed <61875331+camille-marro@users.noreply.github.com>
2024-12-20 16:59:53 +0100

    moved command  in specific function

--- commands/pokemon/main.js
+++ commands/pokemon/main.js
@@ -4,6 +4,74 @@ const { getPlayer, createPlayer, setTimeExplore } = require('../../assets/pokemo
 const { catchPokemon } = require('../../assets/pokemon/addPokemon.js');
 const { exploreGrass } = require('../../assets/pokemon/explore');
 
+async function explore(interaction, player)  {
+    let res = await exploreGrass(player);
+    if (res[0]) {
+        let row = new ActionRowBuilder();
+
+        let msgEmbed = new EmbedBuilder();
+        msgEmbed.setTitle("Vous avez croisé 3 pokémons dans les hautes herbes !");
+        msgEmbed.setColor("Aqua");
+
+        for (let i = 0; i < res[1].length; i++) {
+            let pokemon = res[1][i]["pokemon"];
+            let types = res[1][i]["types"];
+
+            let typeStr = "";
+            for (let type of types) {
+                typeStr += type["name"][0].toUpperCase() + type["name"].substring(1);
+                typeStr += " - ";
+            }
+            typeStr = typeStr.substring(0, typeStr.length - 2);
+
+            let button = new ButtonBuilder()
+                .setCustomId("pokemon" + i)
+                .setLabel("Attraper " + pokemon["name"] + " !")
+                .setStyle(ButtonStyle.Secondary);
+
+            row.addComponents(button);
+            msgEmbed.addFields({name: pokemon["name"], value: typeStr, inline: true});
+        }
+
+        let response = await interaction.reply({
+            content: '',
+            components: [row],
+            embeds: [msgEmbed]
+        });
+
+        const collectorFilter = i => i.user.id === interaction.user.id;
+
+        try {
+            const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
+
+            for (let i = 0; i < res[1].length; i++) {
+                if (confirmation.customId === ('pokemon' + i)) {
+                    let msgEmbed = new EmbedBuilder();
+                    msgEmbed.setTitle("Bravo vous avez attrapé un : " + res[1][i]['pokemon']["name"] + " !");
+                    msgEmbed.setColor("Green");
+
+                    await catchPokemon(player, res[1][i]['pokemon']);
+                    await setTimeExplore(player);
+
+                    await interaction.editReply({
+                        content: '',
+                        components: [],
+                        embeds: [msgEmbed]
+                    });
+                }
+            }
+        } catch (e) {
+            await interaction.editReply({
+                content: 'Aucune confirmation après 1 minute, annulation de la commande.',
+                components: [],
+                embeds: []
+            });
+        }
+    } else {
+        interaction.reply({embeds: [res[1]]});
+    }
+}
+
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pokemon")
@@ -24,72 +92,6 @@ module.exports = {
         }
 
         let option = interaction.options.getString('option');
-        if (option === "explore") {
-            let res = await exploreGrass(player);
-            if (res[0]) {
-                let row = new ActionRowBuilder();
-
-                let msgEmbed = new EmbedBuilder();
-                msgEmbed.setTitle("Vous avez croisé 3 pokémons dans les hautes herbes !");
-                msgEmbed.setColor("Aqua");
-
-                for (let i = 0; i < res[1].length; i++) {
-                    let pokemon = res[1][i]["pokemon"];
-                    let types = res[1][i]["types"];
-
-                    let typeStr = "";
-                    for (let type of types) {
-                        typeStr += type["name"][0].toUpperCase() + type["name"].substring(1);
-                        typeStr += " - ";
-                    }
-                    typeStr = typeStr.substring(0, typeStr.length - 2);
-
-                    let button = new ButtonBuilder()
-                        .setCustomId("pokemon" + i)
-                        .setLabel("Attraper " + pokemon["name"] + " !")
-                        .setStyle(ButtonStyle.Secondary);
-
-                    row.addComponents(button);
-                    msgEmbed.addFields({name: pokemon["name"], value: typeStr, inline: true});
-                }
-
-                let response = await interaction.reply({
-                    content: '',
-                    components: [row],
-                    embeds: [msgEmbed]
-                });
-
-                const collectorFilter = i => i.user.id === interaction.user.id;
-
-                try {
-                    const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 });
-
-                    for (let i = 0; i < res[1].length; i++) {
-                        if (confirmation.customId === ('pokemon' + i)) {
-                            let msgEmbed = new EmbedBuilder();
-                            msgEmbed.setTitle("Bravo vous avez attrapé un : " + res[1][i]['pokemon']["name"] + " !");
-                            msgEmbed.setColor("Green");
-
-                            await catchPokemon(player, res[1][i]['pokemon']);
-                            await setTimeExplore(player);
-
-                            await interaction.editReply({
-                                content: '',
-                                components: [],
-                                embeds: [msgEmbed]
-                            });
-                        }
-                    }
-                } catch (e) {
-                    await interaction.editReply({
-                        content: 'Aucune confirmation après 1 minute, annulation de la commande.',
-                        components: [],
-                        embeds: []
-                    });
-                }
-            } else {
-                interaction.reply({embeds: [res[1]]});
-            }
-        }
+        if (option === "explore") await explore(interaction, player);
     }
 }
