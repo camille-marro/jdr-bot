a9d5c79952c7ffbad86316dc799a9d5b7f016538
GotLed <61875331+camille-marro@users.noreply.github.com>
2024-12-25 23:38:03 +0100

    work now with 0 pokémon

--- assets/pokemon/list.js
+++ assets/pokemon/list.js
@@ -4,6 +4,10 @@ async function list(interaction, player)  {
     let pokemons = await getPlayerPokemons(player);
     let finalStr = "";
 
+    if (pokemons.length < 1) {
+        finalStr = "Vous n'avez aucun pokémon.";
+    }
+
     for (let pokemon of pokemons) {
         if (pokemon["shiny"]) finalStr += ":sparkles: ";
         finalStr += pokemon["name"] + " (" + pokemon["sex"] + ") - niv : " + pokemon["level"]
