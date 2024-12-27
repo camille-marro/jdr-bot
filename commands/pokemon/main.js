98567c92d886468f82b87cc74b085dca524ea48b
GotLed <61875331+camille-marro@users.noreply.github.com>
2024-12-28 00:51:35 +0100

    reworked command to add subcommand

--- commands/pokemon/main.js
+++ commands/pokemon/main.js
@@ -3,17 +3,33 @@ const { SlashCommandBuilder } = require("discord.js");
 const { getPlayer, createPlayer } = require('../../assets/pokemon/handlePlayer.js');
 const { explore } = require('../../assets/pokemon/explore');
 const { list } = require('../../assets/pokemon/list');
+const { train } = require('../../assets/pokemon/train');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("pokemon")
         .setDescription("Jeu pokémon")
-        .addStringOption(option =>
-            option
-                .setName("option")
-                .setDescription("Commande du jeu à faire. Voici la liste : explore, list")
-                .setRequired(true)
+        .addSubcommand(subCommand =>
+            subCommand
+                .setName("explore")
+                .setDescription("Explorer les hautes herbes pour attraper un pokémon")
         )
+        .addSubcommand(subCommand =>
+            subCommand
+                .setName("list")
+                .setDescription("Afficher la liste de ses pokémons")
+        )
+        .addSubcommand(subCommand =>
+            subCommand
+                .setName("train")
+                .setDescription("Entraîner un pokémon")
+                .addStringOption(option =>
+                    option
+                        .setName("pokemon")
+                        .setDescription("Pokémon à entraîner")
+                )
+        )
+
     ,
 
     async execute(interaction) {
@@ -23,8 +39,9 @@ module.exports = {
             await createPlayer(interaction.user.id);
         }
 
-        let option = interaction.options.getString('option');
+        let option = interaction.options.getSubcommand();
         if (option === "explore") await explore(interaction, player);
         else if (option === "list") await list(interaction, player);
+        else if (option === "train") await train(interaction, player)
     }
 }
